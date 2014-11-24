81ed28331bc135ccbccdc99f4ac6457f4bb4c09f
AaronJan <aaronjan@qq.com>
2014-11-24 22:07:22 +0800

    add lib files

---
+++ ArrayTree/Node.php
@@ -0,0 +1,151 @@
+<?php
+namespace ArrayTree;
+
+/**
+ * ArrayTree
+ * 
+ * Convert 2-dimensions array to parent-child relation array
+ * 
+ * Copyright 2014  Aaron Jan <https://github.com/aaronjan>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @version 0.9
+ * @author  Aaron Jan <https://github.com/aaronjan>
+ * @link    https://github.com/aaronjan
+ * @license http://www.apache.org/licenses/LICENSE-2.0 The Apache License V2
+ */
+
+/*
+ * Node
+ * -----------------------------------------------------------------------------
+ * Simple node structure.
+ * 
+ */
+
+class Node {
+    
+    /**
+     * Auto-generated id
+     *
+     * @var string
+     */
+    public $id;
+    
+    /**
+     * Data array that this node carring
+     *
+     * @var array
+     */
+    public $data;
+    
+    /**
+     * This node's parent node
+     *
+     * @var \ArrayTree\Node
+     */
+    protected $_parentNode;
+    
+    /**
+     * Child nodes array
+     *
+     * @var array
+     */
+    protected $_childNodes = array();
+    
+    
+    /**
+     * Constructor
+     *
+     * @return object
+     */
+    public function __construct ($data, $id = false)
+    {
+        $this->data = $data;
+        $this->id = $id ? $id : uniqid('aj_tree', true);
+        
+        return $this;
+    }
+    
+    /**
+     * Attach this node to another node
+     *
+     * @return object
+     */
+    public function attachTo ($parentNode)
+    {
+        $parentNode->appendChild($this);
+    }
+    
+    /**
+     * Set this node's parent node
+     *
+     * @return object
+     */
+    public function setParentNode ($parentNode)
+    {
+        $this->_parentNode = $parentNode;
+    }
+    
+    /**
+     * Append an child node to this node
+     *
+     * @return void
+     */
+    public function appendChild ($childNode)
+    {
+        $this->_childNodes[$childNode->id] = $childNode;
+        $childNode->setParentNode($this);
+    }
+    
+    /**
+     * Detach this node from it's parent node
+     *
+     * @return void
+     */
+    public function detach ()
+    {
+        $this->_parentNode->removeChild($this);
+    }
+    
+    /**
+     * Remove an child node
+     *
+     * @return void
+     */
+    public function removeChild ($childNode)
+    {
+        unset($this->_childNodes[$childNode->id]);
+        $childNode->setParentNode(null);
+    }
+    
+    /**
+     * Get all child nodes
+     *
+     * @return array
+     */
+    public function getAllChildNode ()
+    {
+        return $this->_childNodes;
+    }
+    
+    /**
+     * Get all child nodes
+     *
+     * @return \ArrayTree\Node | bool
+     */
+    public function getParentNode ()
+    {
+        return $this->_parentNode ? $this->_parentNode : false;
+    }
+}
---
+++ ArrayTree/Tree.php
@@ -0,0 +1,352 @@
+<?php
+namespace ArrayTree;
+
+/**
+ * ArrayTree
+ * 
+ * Convert 2-dimensions array to parent-child relation array
+ * 
+ * Copyright 2014  Aaron Jan <https://github.com/aaronjan>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @version 0.9
+ * @author  Aaron Jan <https://github.com/aaronjan>
+ * @link    https://github.com/aaronjan
+ * @license http://www.apache.org/licenses/LICENSE-2.0 The Apache License V2
+ */
+
+class Tree {
+    
+    /**
+     * Data that use for generate array tree
+     *
+     * @var array
+     */
+    protected $_dataList = array();
+    
+    /**
+     * "Id" key name for every data entry
+     *
+     * @var string
+     */
+    protected $_idKey    = 'id';
+    
+    /**
+     * "Parent id" key name for every data entry
+     *
+     * @var string
+     */
+    protected $_parentIdKey = 'parent_id';
+    
+    /**
+     * "Child entries" key name for store child entries
+     *
+     * @var string
+     */
+    protected $_resultChildKey = 'childs';
+    
+    /**
+     * Key name for store this node's parent nodes' ids, set to "false" to disable this function
+     *
+     * @var string/bool
+     */
+    protected $_resultParentIdsKey = 'parent_ids';
+    
+    /**
+     * Whether or not the tree has been builded
+     *
+     * @var bool
+     */
+    protected $_builded   = false;
+    
+    /**
+     * Tree must have an root node (only one), store root node's id key here
+     *
+     * @var mixed
+     */
+    protected $_rootId    = false;
+    
+    /**
+     * Nodes array
+     *
+     * @var array
+     */
+    protected $_nodes      = array();
+    
+    /**
+     * Constructor
+     *
+     * @return object
+     */
+    public function __construct (array $dataList = array())
+    {
+        if (!empty($dataList)) {
+            $this->setData($dataList);
+        }
+        
+        return $this;
+    }
+    
+    /**
+     * Set key name for array's id
+     *
+     * @return void
+     */
+    public function setIdKey ($idKey)
+    {
+        $this->_markChange();
+        $this->_idKey = $idKey;
+    }
+    
+    /**
+     * Set key name for array's parent id
+     *
+     * @return void
+     */
+    public function setParentIdKey ($parentKey)
+    {
+        $this->_markChange();
+        $this->_parentIdKey = $parentKey;
+    }
+    
+    /**
+     * Set key name for store child arrays
+     *
+     * @return void
+     */
+    public function setResultChildKey ($childKey)
+    {
+        $this->_markChange();
+        $this->_resultChildKey = $childKey;
+    }
+    
+    /**
+     * Set key name for store parent nodes' ids
+     *
+     * @return void
+     */
+    public function setResultParentIdsKey ($parentIdsKey)
+    {
+        $this->_markChange();
+        $this->_resultParentIdsKey = $parentIdsKey;
+    }
+    
+    
+    /**
+     * Set data for node generation
+     *
+     * @return void
+     */
+    public function setData ($dataList)
+    {
+        $this->_markChange();
+        $this->_dataList = $dataList;
+    }
+    
+    /**
+     * add a data entry for node generation
+     *
+     * @return void
+     */
+    public function addData ($dataEntry)
+    {
+        $this->_markChange();
+        $this->_dataList[] = $dataEntry;
+    }
+    
+    /**
+     * Convert data to nodes, and then converting nodes to array tree
+     *
+     * @return array
+     */
+    public function getArrayTree ()
+    {
+        if (!$this->_builded) {
+            $this->buildTree();
+        }
+        
+        $arrayTree = $this->_getAllNodeDataRecursively($this->_nodes[$this->_rootId]);
+        return $arrayTree[$this->_resultChildKey];
+    }
+    
+    /**
+     * Convert data to nodes, and then converting nodes to array
+     *
+     * @return array
+     */
+    public function getArray ()
+    {
+        if (!$this->_builded) {
+            $this->buildTree();
+        }
+        
+        $nodeDataArray = array();
+        $childIds      = array_diff(array_keys($this->_nodes), array($this->_rootId));
+        foreach ($childIds as $eachChildId) {
+            $nodeDataArray[] = $this->_nodes[$eachChildId]->data;
+        }
+        
+        return $nodeDataArray;
+    }
+    
+    /**
+     * Convert data arrays to organized nodes
+     *
+     * @return boolean
+     */
+    public function buildTree ()
+    {
+        // reunion dataArray buy "id" value, and get all "parent ids".
+        $reunionedList = array();
+        $parentIds     = array();
+        foreach ($this->_dataList as $eachData) {
+            $parentId                                = $eachData[$this->_parentIdKey];
+            $parentIds[]                             = $parentId;
+            $reunionedList[$eachData[$this->_idKey]] = $eachData;
+        }
+        $parentIds = array_unique($parentIds);
+        // get root node id (only one)
+        $rootIds   = array_diff($parentIds, array_keys($reunionedList));
+        if (count($rootIds) != 1) {
+            throw new Exception('Can only have one root node.');
+        }
+        $this->_rootId = array_pop($rootIds);
+        // convert data to node objects
+        $this->_generateNode($reunionedList);
+        $this->_organizeNode();
+
+        return true;
+    }
+    
+    /**
+     * Generate node objects using reunioned array
+     *
+     * @return boolean
+     */
+    protected function _generateNode ($reunionedList)
+    {
+        $this->_nodes = array();
+        $this->_nodes[$this->_rootId] = new Node(array(), $this->_rootId);
+        foreach ($reunionedList as $key => $eachData) {
+            $this->_nodes[$key] = new Node($eachData, $key);
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Organize node objects, make connections
+     *
+     * @return boolean
+     */
+    protected function _organizeNode ()
+    {
+        //Get all child nodes' keys except root node
+        $childIds = array_diff(array_keys($this->_nodes), array($this->_rootId));
+        
+        //Connect all nodes
+        foreach ($childIds as $eachId) {
+            $childNode  = $this->_nodes[$eachId];
+            $parentId   = $childNode->data[$this->_parentIdKey];
+            $parentNode = $this->_nodes[$parentId];
+            $childNode->attachTo($parentNode);
+        }
+        //Setting all nodes' parent ids
+        if ($this->_resultParentIdsKey) {
+            $rootNodeId = $this->_rootId;
+            foreach ($childIds as $eachId) {
+                $childNode                             = $this->_nodes[$eachId];
+                $parentIds                             = $this->_getNodeParentIdRecursively($childNode, $rootNodeId);
+                $childNode->data[$this->_resultParentIdsKey] = $parentIds;
+            }
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Get target node's array tree (recursively)
+     *
+     * @return array
+     */
+    protected function _getNodeParentIdRecursively ($node, $rootNodeId, $callOnDemand = true)
+    {
+        $returnValue = array();
+        
+        $parentNode = $node->getParentNode();
+        if ($parentNode === false) {
+            throw new Exception('Oops, seems there\'s one node hasn\'t parent node, and it isn\'t root node.');
+        } elseif ($parentNode->id == $rootNodeId) {
+            return array();
+        }
+        
+        $returnValue[] = $parentNode->id;
+        $returnValue[] = $this->_getNodeParentIdRecursively($parentNode, $rootNodeId, false);
+        
+        if ($callOnDemand) {
+            $returnValue = $this->_flattenArray($returnValue);
+        }
+        
+        return $returnValue;
+    }
+    
+    /**
+     * Get target node's array tree (recursively)
+     *
+     * @return array
+     */
+    protected function _getAllNodeDataRecursively ($treeNode)
+    {
+        $returnArr = $treeNode->data;
+        $returnArr[$this->_resultChildKey] = array();
+        $childNodeList = $treeNode->getAllChildNode();
+        
+        if (!empty($childNodeList)) {
+            foreach ($childNodeList as $eachChildNode) {
+                $returnArr[$this->_resultChildKey][] = $this->_getAllNodeDataRecursively($eachChildNode);
+            }
+        }
+        
+        return $returnArr;
+    }
+    
+    /**
+     * Flat an array (recursively)
+     *
+     * @return array
+     */
+    protected function _flattenArray ($array)
+    {
+        $returnArray = array();
+        $flattenFunc = function ($item, $key, $flatArray) {
+            if (!is_array($item)) {
+                $flatArray[] = $item;
+            }
+        };
+        array_walk_recursive($array, $flattenFunc, &$returnArray);
+        
+        return $returnArray;
+    }
+    
+    /**
+     * Mark changes for regenerate nodes
+     *
+     * @return void
+     */
+    protected function _markChange ()
+    {
+        $this->_builded = false;
+    }
+    
+}
